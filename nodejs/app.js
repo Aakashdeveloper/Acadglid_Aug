0adf9f0b43871419b518ffa81eb594b1686a5c4b
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-09-16 10:13:16 +0530

    A

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,26 +3,25 @@ let app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', './src/views');
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 let nav = [
     {link: '/', title: 'Home'},
     {link: '/movies', title: 'Movies'},
-    {link: '/admin', title: 'Admin'}
+    {link: '/artist', title: 'Artist'}
 ]
+
+let moviesRouter = require('./src/routes/moviesRoutes')(nav);
+let artistRouter = require('./src/routes/ArtistRoutes')(nav);
+
+app.use('/movies', moviesRouter);
+app.use('/artist', artistRouter);
 
 app.get('/',(req,res) => { 
     res.render('index',{
         title: 'Home Page',
         nav:nav
 
-    })
-})
-
-app.get('/movies', (req,res) => {
-    res.render('movies',{
-        title: 'Movies Page',
-        nav
     })
 })
 
